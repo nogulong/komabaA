1ddab1fb290afd051d081ce9f25e1a52553f7393
milkywaytetr4 <kusatsu.800@gmail.com>
2024-12-06 18:27:02 +0900

    optimizerを実装

--- optimizer.py
+++ optimizer.py
@@ -38,7 +38,7 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
+                
 
                 # State should be stored in this dictionary
                 state = self.state[p]
@@ -48,13 +48,24 @@ class AdamW(Optimizer):
 
                 # Update first and second moments of the gradients
 
-                # Bias correction
-                # Please note that we are using the "efficient version" given in
-                # https://arxiv.org/abs/1412.6980
+                beta1, beta2 = group["betas"]
+                eps = group["eps"]
+                weight_decay = group["weight_decay"]
+                correct_bias = group["correct_bias"]
 
-                # Update parameters
+                if len(state) == 0:
+                    state['count'] = 0
+                    state['m'] = torch.zeros_like(p)      # 一次モーメント
+                    state['v'] = torch.zeros_like(p)      # 二次モーメント
+                
+                state['count'] += 1
+                state['m'] = beta1 * state['m'] + (1-beta1) * grad
+                state['v'] = beta2 * state['v'] + (1-beta2) * grad**2
 
-                # Add weight decay after the main gradient-based updates.
-                # Please note that the learning rate should be incorporated into this update.
-
+                if correct_bias:
+                    m_hat = state['m'] / (1 - beta1 ** state['count'])
+                    v_hat = state['v'] / (1 - beta2 ** state['count'])
+                    p.data = (1 - alpha * weight_decay) * p.data - alpha * m_hat / (v_hat.sqrt() + eps)
+                else:
+                    p.data = (1 - alpha * weight_decay) * p.data - alpha * state['m'] / (state['v'].sqrt() + eps)
         return loss
